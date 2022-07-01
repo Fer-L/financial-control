750b1a54b9099c0d32b35c3c82e362bf1574f95c
Leandrovargas0 <leandroviniciusvargas@gmail.com>
2022-07-01 10:25:59 -0400

    autenticação e criação de usuário

--- src/main/java/com/web/finances/api/dto/LoginDTO.java
+++ src/main/java/com/web/finances/api/dto/LoginDTO.java
@@ -1,9 +1,13 @@
 package com.web.finances.api.dto;
 
+import com.web.finances.domain.model.Company;
+import com.web.finances.domain.model.Users;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
+  private Long id;
   private String username;
   private String password;
+  private String name;
 }
